d4feb6f7590429efafa28832de800e0474588a08
xianlin-zhao <1900013063@pku.edu.cn>
2026-01-27 20:19:30 +0800

    chores: change some paths for experiments.

--- src/graph/analyze_missing_code_path.py
+++ src/graph/analyze_missing_code_path.py
@@ -10,13 +10,13 @@ from collections import defaultdict, deque
 # FILTERED_PATH = "/data/data_public/riverbag/testRepoSummaryOut/mrjob/1:3/filtered.jsonl"
 # OUTPUT_GRAPH_PATH = "/data/zxl/Search2026/outputData/devEvalSearchOut/System_mrjob/0108/graph_results"
 
-METHODS_CSV = "/data/data_public/riverbag/testRepoSummaryOut/Filited/alembic/methods.csv"
-ENRE_JSON = "/data/data_public/riverbag/testRepoSummaryOut/Filited/alembic/alembic-report-enre.json"
-FILTERED_PATH = "/data/data_public/riverbag/testRepoSummaryOut/Filited/alembic/filtered.jsonl"
-OUTPUT_GRAPH_PATH = "/data/zxl/Search2026/outputData/devEvalSearchOut/Database_alembic/0115/graph_results"
+METHODS_CSV = "/data/data_public/riverbag/testRepoSummaryOut/Filited/mrjob/methods.csv"
+ENRE_JSON = "/data/data_public/riverbag/testRepoSummaryOut/Filited/mrjob/mrjob-report-enre.json"
+FILTERED_PATH = "/data/data_public/riverbag/testRepoSummaryOut/Filited/mrjob/filtered.jsonl"
+OUTPUT_GRAPH_PATH = "/data/zxl/Search2026/outputData/devEvalSearchOut/System_mrjob/0115/graph_results"
 
 REMOVE_FIRST_DOT_PREFIX = False
-PREFIX = "alembic"  # 如果移除前缀的选项为True，这里记得指定项目的名称作为前缀
+PREFIX = "mrjob"  # 如果移除前缀的选项为True，这里记得指定项目的名称作为前缀
 
 def load_methods_csv(csv_path):
     df = pd.read_csv(csv_path)
